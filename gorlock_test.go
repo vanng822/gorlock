215d0701fba458a09ec5dd0b42b60b66c0c96735
Van Nhu Nguyen <nguyen.van.nhu@vnmedia.se>
2018-01-27 18:39:54 +0100

    More tests

--- gorlock_test.go
+++ gorlock_test.go
@@ -5,6 +5,7 @@ import (
 	"testing"
 	"time"
 
+	"github.com/gpitfield/redlock"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -20,28 +21,34 @@ func TestRunError(t *testing.T) {
 	}), "run is not ok")
 }
 
-func doBlock(key string, timeout time.Duration) {
+func doBlock(key string, timeout time.Duration, done chan bool) {
+	// should use Lock directly
 	l, _ := Acquire(key, DefaultSettings)
 	go func() {
 		time.Sleep(timeout)
 		l.Unlock(key)
+		done <- true
 	}()
 }
 
 func TestRunWaitingOk(t *testing.T) {
 	key := "runwating.ok"
-	doBlock(key, LockWaitingDefaultSettings.RetryInterval*2)
+	done := make(chan bool)
+	doBlock(key, LockWaitingDefaultSettings.RetryInterval*2, done)
 	assert.Nil(t, RunWaiting(key, func() error {
 		return nil
 	}))
+	<-done
 }
 
 func TestRunWaitingError(t *testing.T) {
 	key := "runwating.error"
-	doBlock(key, LockWaitingDefaultSettings.RetryInterval*2)
+	done := make(chan bool)
+	doBlock(key, LockWaitingDefaultSettings.RetryInterval*2, done)
 	assert.EqualError(t, RunWaiting(key, func() error {
 		return fmt.Errorf("run wating is not ok")
 	}), "run wating is not ok")
+	<-done
 }
 
 func TestLockTimesUp(t *testing.T) {
@@ -51,9 +58,40 @@ func TestLockTimesUp(t *testing.T) {
 	}()
 	LockWaitingDefaultSettings.RetryTimeout = 300 * time.Millisecond
 	key := "runwating.error"
-	doBlock(key, 500*time.Millisecond)
+	done := make(chan bool)
+	doBlock(key, 500*time.Millisecond, done)
 	assert.EqualError(t, RunWaiting(key, func() error {
 		return nil
 	}), "Time's up! Can not acquire lock key: runwating.error")
+	<-done
+}
 
+func TestConnectionError(t *testing.T) {
+	tmp := *_redisConfig
+	defer func() {
+		*_redisConfig = tmp
+	}()
+	key := "runwating.error"
+	done := make(chan bool)
+	doBlock(key, 100*time.Millisecond, done)
+	_redisConfig = &redlock.RedisConfig{
+		Address:        "localhost:6390",
+		Database:       1,
+		KeyPrefix:      "gorlock",
+		ConnectTimeout: 30 * time.Second,
+	}
+	lock, err := Acquire(key, DefaultSettings)
+	assert.Nil(t, lock)
+	assert.EqualError(t, err, "dial tcp [::1]:6390: getsockopt: connection refused")
+	<-done
+}
+
+func TestCanNotAcquire(t *testing.T) {
+	key := "runwating.error"
+	done := make(chan bool)
+	doBlock(key, 100*time.Millisecond, done)
+	lock, err := Acquire(key, DefaultSettings)
+	assert.Nil(t, lock)
+	assert.EqualError(t, err, "Can not acquire lock key: runwating.error")
+	<-done
 }
